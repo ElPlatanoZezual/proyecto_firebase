55dd9e858f2375938ffb4cd90adc87c61f1f9f15
ElPlatanoZezual <89117490+ElPlatanoZezual@users.noreply.github.com>
2023-11-25 04:16:06 -0300

    booleanos

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -31,156 +31,170 @@ class _HomePageState extends State<HomePage> {
     super.didChangeDependencies();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text(
-          'Eventos Potasio',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24.0,
-          ),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      backgroundColor: Colors.purple,
+      title: Text(
+        'Eventos Potasio',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24.0,
         ),
       ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.purple,
-              ),
-              child: Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    loginProvider.singInGoogle();
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.purple,
-                  ),
-                  child: Text(
-                    'Iniciar sesión de admin',
-                    style: TextStyle(
-                      color: Colors.purple,
-                      fontSize: 16.0,
-                    ),
+    ),
+    drawer: Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.purple,
+            ),
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (loginProvider.isLoggedIn()) {
+                      // Cerrar sesión
+                      loginProvider.signOut();
+                    } else {
+                      // Iniciar sesión
+                      loginProvider.signInGoogle();
+                    }
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.purple,
+                ),
+                child: Text(
+                  loginProvider.isLoggedIn()
+                      ? 'Cerrar sesión'
+                      : 'Iniciar sesión',
+                  style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 16.0,
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      body: FutureBuilder(
-        future: eventosProvider.getEventos(),
-        builder: ((context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _selectedTileIndex = index;
-                    });
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetallesEventoPage(
-                          evento: snapshot.data?[index],
-                        ),
+    ),
+    body: FutureBuilder(
+      future: eventosProvider.getEventos(),
+      builder: ((context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data?.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedTileIndex = index;
+                  });
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetallesEventoPage(
+                        evento: snapshot.data?[index],
                       ),
-                    );
-                  },
-                  child: Card(
-                    elevation: 4.0,
-                    margin: EdgeInsets.all(8.0),
-                    color: _selectedTileIndex == index
-                        ? Colors.grey[200]
-                        : Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                    child: ListTile(
-                      title: Text(
-                        snapshot.data?[index]['nombre'] ?? '',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
+                  );
+                },
+                child: Card(
+                  elevation: 4.0,
+                  margin: EdgeInsets.all(8.0),
+                  color: _selectedTileIndex == index
+                      ? Colors.grey[200]
+                      : Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      snapshot.data?[index]['nombre'] ?? '',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    subtitle: Text(
+                      snapshot.data?[index]['descripcion'] ?? '',
+                    ),
+                    leading: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () async {
+                            likesContador =
+                                (snapshot.data?[index]['likes'] ?? 0) + 1;
+                            await eventosProvider.sumarLikes(
+                                snapshot.data?[index]['id'], likesContador);
+                            setState(() {});
+                          },
+                          child: Icon(
+                            MdiIcons.heart,
+                            color: Colors.purple,
+                            size: 28.0,
+                          ),
                         ),
-                      ),
-                      subtitle: Text(
-                        snapshot.data?[index]['descripcion'] ?? '',
-                      ),
-                      leading: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          GestureDetector(
-                            onTap: () async {
-                              likesContador =
-                                  (snapshot.data?[index]['likes'] ?? 0) + 1;
-                              await eventosProvider.sumarLikes(
-                                  snapshot.data?[index]['id'], likesContador);
-                              setState(() {});
+                        SizedBox(height: 4.0),
+                        Text(
+                          (snapshot.data?[index]['likes'] ?? 0).toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple,
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: loginProvider.isLoggedIn()
+                        ? GestureDetector(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/editar', arguments: {
+                                "nombre": Text(snapshot.data?[index]['nombre'] ?? ''),
+                                "descripcion":
+                                    Text(snapshot.data?[index]['descripcion'] ?? ''),
+                                "lugar": Text(snapshot.data?[index]['lugar'] ?? ''),
+                                "tipo": Text(snapshot.data?[index]['tipo'] ?? ''),
+                                "likes": snapshot.data?[index]['likes'] ?? 0,
+                                "fecha": snapshot.data?[index]['fecha'] ?? '',
+                                "id": snapshot.data?[index]['id'] ?? '',
+                              });
                             },
                             child: Icon(
-                              MdiIcons.heart,
+                              MdiIcons.fileEdit,
                               color: Colors.purple,
                               size: 28.0,
                             ),
-                          ),
-                          SizedBox(height: 4.0),
-                          Text(
-                            (snapshot.data?[index]['likes'] ?? 0).toString(),
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.purple,
-                              fontSize: 14.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                      trailing: GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/editar', arguments: {
-                            "nombre": Text(snapshot.data?[index]['nombre'] ?? ''),
-                            "descripcion":
-                                Text(snapshot.data?[index]['descripcion'] ?? ''),
-                            "lugar": Text(snapshot.data?[index]['lugar'] ?? ''),
-                            "tipo": Text(snapshot.data?[index]['tipo'] ?? ''),
-                            "likes": snapshot.data?[index]['likes'] ?? 0,
-                            "fecha": snapshot.data?[index]['fecha'] ?? '',
-                            "id": snapshot.data?[index]['id'] ?? '',
-                          });
-                        },
-                        child: Icon(
-                          MdiIcons.fileEdit,
-                          color: Colors.purple,
-                          size: 28.0,
-                        ),
-                      ),
-                    ),
+                          )
+                        : null,
                   ),
-                );
-              },
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        }),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/agregar');
-        },
-        backgroundColor: Colors.purple,
-        child: Icon(
-          Icons.add,
-          size: 32.0,
-        ),
-      ),
-    );
-  }
+                ),
+              );
+            },
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      }),
+    ),
+    floatingActionButton: loginProvider.isLoggedIn()
+        ? FloatingActionButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/agregar');
+            },
+            backgroundColor: Colors.purple,
+            child: Icon(
+              Icons.add,
+              size: 32.0,
+            ),
+          )
+        : null,
+  );
+}
 }
--- lib/providers/login_provider.dart
+++ lib/providers/login_provider.dart
@@ -5,24 +5,40 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+class LoginProvider with ChangeNotifier {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  var firebaseUser = FirebaseAuth.instance.currentUser;
+  GoogleSignIn googleSignIn = GoogleSignIn();
 
-class LoginProvider with ChangeNotifier{
-    FirebaseFirestore db = FirebaseFirestore.instance;
-    FirebaseAuth auth = FirebaseAuth.instance;
-    var firebaseUser = FirebaseAuth.instance.currentUser;
-    GoogleSignIn googleSingIn = GoogleSignIn();
-    Future <void> singInGoogle () async {
-      try {
-        final GoogleSignInAccount? account = await googleSingIn.signIn();
-        final GoogleSignInAuthentication googleAuth = await account!.authentication;
-        final AuthCredential credencial = await GoogleAuthProvider.credential(
-          accessToken: googleAuth.accessToken, idToken: googleAuth.idToken
-        );
-        firebaseUser = (await auth.signInWithCredential(credencial)).user;
-        await db.collection("users").doc(firebaseUser?.uid).set({'correoElectronico': firebaseUser?.email,'nombre': firebaseUser?.displayName, 'id':firebaseUser?.uid, 'foto':firebaseUser?.photoURL});
-      } on FirebaseAuthException catch (e) {
-        log('${e.message}');
-      }
-      notifyListeners();
+  Future<void> signInGoogle() async {
+    try {
+      final GoogleSignInAccount? account = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleAuth = await account!.authentication;
+      final AuthCredential credencial = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      firebaseUser = (await auth.signInWithCredential(credencial)).user;
+      await db.collection("users").doc(firebaseUser?.uid).set({
+        'correoElectronico': firebaseUser?.email,
+        'nombre': firebaseUser?.displayName,
+        'id': firebaseUser?.uid,
+        'foto': firebaseUser?.photoURL,
+      });
+    } on FirebaseAuthException catch (e) {
+      log('${e.message}');
     }
+  }
+
+  Future<void> signOut() async {
+    await auth.signOut();
+    await googleSignIn.signOut();
+    firebaseUser = null;
+    notifyListeners();
+  }
+
+  bool isLoggedIn() {
+    return firebaseUser != null;
+  }
 }
